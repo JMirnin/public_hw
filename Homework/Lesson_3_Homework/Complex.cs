aacf2139f9698fce3843b8b61bfa8d7b443dfbd5
JMirnin <jmirnin@yandex.ru>
2019-09-30 21:29:33 +0300

    ДЗ 3

---
+++ Homework/Lesson_3_Homework/Complex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Homework
+{
+    class Complex
+    {
+        public double a, b;
+
+        public Complex(double A, double B)
+        {
+            a = A;
+            b = B;
+        }
+
+        public static Complex Sum(Complex Z1, Complex Z2)
+        {
+            //z=(a1+a2)+(b1+b2)i
+            return new Complex(Z1.a + Z2.a, Z1.b + Z2.b);
+        }
+
+        public static Complex Minus(Complex Z1, Complex Z2)
+        {
+            ////z=(a1-a2)+(b1-b2)i
+            return new Complex(Z1.a - Z2.a, Z1.b - Z2.b);
+        }
+
+        public static Complex Mult(Complex Z1, Complex Z2)
+        {
+            //(a1a2−b1b2)+(a1b2+b1a2)i
+            return new Complex(Z1.a * Z2.a - Z1.b * Z2.b, Z1.a * Z2.b + Z1.b * Z2.a);
+        }
+
+        /// <summary>
+        /// Вывод комплексного числа
+        /// </summary>
+        /// <returns></returns>
+        public string Print()
+        {
+            if (b < 0)
+            {
+                return $"{a} - {Math.Abs(b)}i";
+            }
+            else
+            {
+                return $"{a} + {b}i";
+            }
+        }
+    }
+
+}
---
+++ Homework/Lesson_3_Homework/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Анатолий Толстых.
+
+            //Задание 1.
+//            а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
+//            б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+//            в) Добавить диалог с использованием switch демонстрирующий работу класса.
+
+
+
+            Complex complex1 = new Complex(2, 3);
+            Complex complex2 = new Complex(-1, 1);
+
+            Console.WriteLine($"Имеются комплексные числа {complex1.Print()} и {complex2.Print()}. \n" +
+                $"Нажмите '+', чтобы сложить их, '-' чтобы вычесть и '*' чтобы перемножить.");
+            var userKey = Console.ReadLine();
+
+            switch (userKey)
+            {
+                case "+": Console.WriteLine($"({complex1.Print()}) + ({complex2.Print()}) = {Complex.Sum(complex1, complex2).Print()}");
+                    break;
+                case "-": Console.WriteLine($"({complex1.Print()}) - ({complex2.Print()}) = {Complex.Minus(complex1, complex2).Print()}");
+                    break;
+                case "*": Console.WriteLine($"({complex1.Print()}) * ({complex2.Print()}) = {Complex.Mult(complex1, complex2).Print()}");
+                    break;
+                default: Console.WriteLine("Вы нажали какую-то ерунду");
+                    break;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
