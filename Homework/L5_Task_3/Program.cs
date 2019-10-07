983b800936e1ccbf3d88512a1b4f0253aa870230
JMirnin <jmirnin@yandex.ru>
2019-10-08 01:23:20 +0300

    ДЗ 5

---
+++ Homework/L5_Task_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace L5_Task_3
+{
+
+    //  Анатолий Толстых
+
+    //  Задание 3.
+    //  * Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+    //  Например:
+    //  badc являются перестановкой abcd.
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите первую строку: ");
+            string string1 = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string string2 = Console.ReadLine();
+
+            bool ident = CheckSymbols(string1, string2);
+
+            if (ident)
+            {
+                Console.WriteLine("Строки являются перестановкой друг друга.");
+            }
+            else
+            {
+                Console.WriteLine("Строки не переставлены, они разные.");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool CheckSymbols(string string1, string string2)
+        {
+            Regex regex = new Regex(@"(.)"); //разбиваем на массив символов
+            string[] array1 = regex.Split(string1);
+            string[] array2 = regex.Split(string2);
+
+            //сортируем массив
+            Array.Sort(array1);
+            Array.Sort(array2);
+
+            //составляем в новые строки и сравниваем
+            return (String.Join("", array1) == String.Join("", array2));
+
+        }
+    }
+}
---
+++ Homework/Lesson_5_Homework/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lesson_5_Homework
+{
+    //Анатолий Толстых
+
+    //Задание 1.
+    //    Создать программу, которая будет проверять корректность ввода логина.
+    //    Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+    //    б) ** с использованием регулярных выражений.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Regex logEx = new Regex(@"^[a-zA-Z]{1}[a-zA-Z0-9]{1,9}$");
+
+            Console.Write("Введите логин: ");
+            if (logEx.IsMatch(Console.ReadLine())) 
+            {
+                Console.WriteLine("Логин корректный");
+            }
+            else
+            {
+                Console.WriteLine("Логин некорретный");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
